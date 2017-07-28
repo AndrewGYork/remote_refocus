fec1a2e2f9d8ecc16d7724d81872ae0bcc79d36e
Alfred Millett-Sikking <amsikking@gmail.com>
2017-07-28 13:49:50 -0700

    Add files via upload

---
+++ figure_generation/figure_8_graticule.py
@@ -0,0 +1,167 @@
+import os
+import subprocess as sp
+from shutil import copyfile
+import numpy as np
+import matplotlib.pyplot as plt
+import np_tif
+
+## Set/create directories
+input_directory = os.path.abspath(os.path.join(
+    os.getcwd(), os.pardir, os.pardir, 'big_data_input', 'Graticule'))
+temp_directory = os.path.abspath(os.path.join(
+    os.getcwd(), os.pardir, os.pardir, 'temp'))
+if not os.path.isdir(temp_directory): os.mkdir(temp_directory)
+temp_directory = os.path.join(temp_directory, 'Graticule')
+if not os.path.isdir(temp_directory): os.mkdir(temp_directory)
+output_directory = os.path.abspath(os.path.join(
+    os.getcwd(), os.pardir, os.pardir, 'big_data_output'))
+if not os.path.isdir(output_directory): os.mkdir(output_directory)
+output_directory = os.path.join(output_directory, 'Graticule')
+if not os.path.isdir(output_directory): os.mkdir(output_directory)
+
+## Set input file name and acquisition parameters for processing
+input_filename = ('transmitted_z_step_10_graticule.tif')
+cropped_filename = os.path.splitext(input_filename)[0] + '_cropped.tif'
+input_filename = os.path.join(input_directory, input_filename)
+cropped_filename = os.path.join(temp_directory, cropped_filename)
+num_tps = 1000 # Number of time points in series
+left_crop = 500
+right_crop = 650
+top_crop = 0
+bottom_crop = 0
+
+## If cropped file exists then load
+if os.path.exists(os.path.join(temp_directory, cropped_filename)):
+    print('Found cropped tif, loading...', end='', sep='')
+    data = np_tif.tif_to_array(cropped_filename)
+    print('done')
+    print('tif shape (t, y, x) =', data.shape)
+
+## If no file found then load original and crop
+else:
+    print('Loading original file...', end='', sep='')
+    data = np_tif.tif_to_array(input_filename)
+    print('done')
+    data = data.reshape((num_tps,) + data.shape[-2:])
+    print('tif shape (t, y, x) =', data.shape)
+    print('Cropping...', end='', sep='')
+    if left_crop or right_crop > 0:
+        data = data[:, :, left_crop:-right_crop]
+    if top_crop or bottom_crop > 0:
+        data = data[:, top_crop:-bottom_crop, :]
+    print('done')
+    print("Saving result...", end='', sep='')
+    np_tif.array_to_tif(
+        data.reshape(num_tps, data.shape[-2], data.shape[-1]),
+        cropped_filename, slices=1, channels=1, frames=num_tps)
+    print('done')
+    print('tif shape (t, y, x) =', data.shape)
+
+## Choose parameters for video
+current_frame = -1
+xmargin = 0.01
+ymargin = 0.025
+space = 0.175
+img_size = 0.5
+max_intensity = 10000
+wlw = 2 # white line width half amplitude
+start_tp = 0 # removing uneventful begging
+stop_tp = 100 # remove uneventful end
+xslice = 80 # choose slice along x for display
+
+## Set output folder for images to make video
+output_filename = os.path.join(temp_directory, 'img%06i.png')
+
+## Make images for video
+fig = plt.figure()
+for t in range(wlw + start_tp, num_tps - wlw - stop_tp):
+    plt.clf()
+    current_frame += 1
+    time = 0.001*t - 0.002 - 0.001*start_tp
+    print('time = ', time)
+    ax1 = plt.axes([0, 0.575, img_size, img_size])
+    ax1_data = data[t, :, :]
+    ax1.imshow(ax1_data, cmap='gray', vmin=0, vmax=max_intensity)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax1.get_yticklabels(), visible=False)
+    plt.setp(ax1.get_xticklines(), visible=False)
+    plt.setp(ax1.get_yticklines(), visible=False)
+    ax2 = plt.axes([0, 0, img_size, 1.5*img_size])
+    ax2_data = np.zeros((data.shape[0], data.shape[2], 3)) # RGB
+    make_me_rgb = data[:, xslice, :].reshape(data.shape[0], data.shape[2], 1)
+    make_me_rgb = np.clip(make_me_rgb/max_intensity, 0, 1) # Normalize to 0, 1
+    ax2_data[:, :, :] = make_me_rgb # Broadcast across all three color channels
+    white_line = ax2_data[t-wlw:t+wlw, :, :]
+    yellow = np.array([1, 1, 0])
+    white_line[0, :, :] = 0.5*yellow # Woo, broadcasting.
+    white_line[1, :, :] = 1.0*yellow
+    white_line[2, :, :] = 1.0*yellow
+    white_line[3, :, :] = 0.5*yellow
+    ax2.imshow(ax2_data)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_yticklabels(), visible=False)
+    plt.setp(ax2.get_xticklines(), visible=False)
+    plt.setp(ax2.get_yticklines(), visible=False)
+    if 150 < t < 375: 
+        plt.figtext(xmargin, ymargin + 3.9*space, 'Microscope',
+                    color='yellow', family='monospace')
+    else:
+        plt.figtext(xmargin, ymargin + 3.9*space, 'Microscope',
+                    color='white', family='monospace')
+    if 475 < t < 625: 
+        plt.figtext(xmargin, ymargin + 2*space, 'Stage',
+                    color='yellow', family='monospace')
+    else:
+        plt.figtext(xmargin, ymargin + 2*space, 'Stage',
+                    color='white', family='monospace')
+    if 725 < t < 800: 
+        plt.figtext(xmargin, ymargin + space, 'Remote',
+                    color='yellow', family='monospace')
+    else:
+        plt.figtext(xmargin, ymargin + space, 'Remote',
+                    color='white', family='monospace')
+    plt.figtext(xmargin, ymargin, 't=%6ss'%('%0.3f'%time),
+                color='yellow', family='monospace')
+    plt.savefig(output_filename%current_frame, bbox_inches='tight')
+plt.close(fig)
+
+## Choose 'poster' image and copy to video location
+copyfile(os.path.join(temp_directory, 'img000250.png'),
+         os.path.join(output_directory, 'poster.png'))
+
+## Make video from images
+print("Converting images to mp4...", end='')
+convert_command = [
+   'ffmpeg', '-y',              # auto overwrite files
+   '-r', '50',                  # frame rate
+   '-f', 'image2',              # format is image sequence
+   '-i', os.path.join(temp_directory,
+                      'img%06d.png'),  # image sequence name
+   '-movflags', 'faststart',    # internet optimisation...(?)
+   '-pix_fmt', 'yuv420p',       # cross browser compatibility
+   '-vcodec', 'libx264',        # codec choice
+   '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2',  # even pixel number (important)
+   '-preset', 'veryslow',       # take time and compress to max
+   '-crf', '25',                # image quality vs file size
+   os.path.join(output_directory, 'figure.mp4')]  # output file name
+try:
+   with open('conversion_messages.txt', 'wt') as f:
+       f.write("So far, everthing's fine...\n")
+       f.flush()
+       sp.check_call(convert_command, stderr=f, stdout=f)
+       f.flush()
+   os.remove('conversion_messages.txt')
+except: # This is unlikely to be platform independent :D
+   print("MP4 conversion failed. Is ffmpeg installed?")
+   raise
+print('"figure.mp4" created')
+print('done.')
+
+## This is a default figure so copy to 'images' in 'master' directory
+image_directory = os.path.abspath(os.path.join(
+    os.getcwd(), os.pardir, os.pardir, 'master', 'images', 'Graticule'))
+if not os.path.isdir(image_directory): os.mkdir(image_directory)
+copyfile(os.path.join(output_directory, 'figure.mp4'),
+         os.path.join(image_directory, 'figure.mp4'))
+copyfile(os.path.join(output_directory, 'poster.png'),
+         os.path.join(image_directory, 'poster.png'))
---
+++ figure_generation/figure_9_yeast_2_color.py
@@ -0,0 +1,228 @@
+import os
+import subprocess as sp
+from shutil import copyfile
+import numpy as np
+import matplotlib.pyplot as plt
+import np_tif
+
+## Set/create directories
+input_directory = os.path.abspath(os.path.join(
+    os.getcwd(), os.pardir, os.pardir, 'big_data_input', 'Yeast_2_color'))
+temp_directory = os.path.abspath(os.path.join(
+    os.getcwd(), os.pardir, os.pardir, 'temp'))
+if not os.path.isdir(temp_directory): os.mkdir(temp_directory)
+temp_directory = os.path.join(temp_directory, 'Yeast_2_color')
+if not os.path.isdir(temp_directory): os.mkdir(temp_directory)
+output_directory = os.path.abspath(os.path.join(
+    os.getcwd(), os.pardir, os.pardir, 'big_data_output'))
+if not os.path.isdir(output_directory): os.mkdir(output_directory)
+output_directory = os.path.join(output_directory, 'Yeast_2_color')
+if not os.path.isdir(output_directory): os.mkdir(output_directory)
+
+## Set input file name and acquisition parameters for processing
+input_filename = (
+    'Yeast_large_FOV-spx_cyan_25_vol_12um_stk_1um_step_10ms_exp8-013.tif')
+re_ordered_filename = os.path.splitext(input_filename)[0] + '_re_ordered.tif'
+input_filename = os.path.join(input_directory, input_filename)
+re_ordered_filename = os.path.join(temp_directory, re_ordered_filename)
+num_channels = 2 # Number of colors
+num_slices = 13 # Number of z stack slices
+num_tps = 25 # Number of volumes or time points in series
+
+## If re-ordered file exists then skip re-ordering and cropping
+if os.path.exists(re_ordered_filename):
+    print('Found re-ordered tif, loading...', end='', sep='')
+    data = np_tif.tif_to_array(re_ordered_filename)
+    data = data.reshape((num_tps, num_slices, num_channels) + data.shape[-2:])
+    print('done')
+    print('tif shape (t, z, c, y, x) =', data.shape)
+## If no registered file is found then process original data file
+else:
+    print('Loading tif...', end='', sep='')
+    data = np_tif.tif_to_array(input_filename)
+    data = data.reshape((num_tps, num_slices, num_channels) + data.shape[-2:])
+    print('done')
+    print('tif shape (t, z, c, y, x) =', data.shape)
+    # Since our volumes were taken bidirectionally we also need to fix
+    # the order.
+    print('Re-ordering...', end='', sep='')
+    for which_t in range(num_tps):
+        # Flip every other volume in the Z-direction
+        if which_t % 2 == 0:
+            data[which_t, :, :, :, :] = data[which_t, ::-1, :, :, :]
+    print('done')
+    print("Saving result...", end='')
+    np_tif.array_to_tif(
+        data.reshape(num_tps*num_channels*num_slices, data.shape[-2],
+                     data.shape[-1]), re_ordered_filename,
+                     slices=num_slices, channels=num_channels, frames=num_tps)
+    print('done')
+    print('tif shape (t, z, c, y, x) =', data.shape)
+
+## Choose parameters for video
+num_z_stack = 2
+pause = 2
+z_slow_down_factor = 1
+z_scale = 1
+current_frame = -1
+current_tp = -1
+xmargin = 0.15
+ymargin = 0.6
+space = 0.0275
+min_int = 1000
+max_int = 40000
+
+## Set output folder and filename for images to make video
+output_filename = os.path.join(temp_directory, 'img%06i.png')
+
+## Define normalisation function for RGB conversion
+data0_max = 25000
+def norm0(x):
+        if data0_max == x.min(): return np.zeros_like(x)
+        return (x - x.min()) / (data0_max - x.min())
+data1_max = 5000
+def norm1(x):
+        if data1_max == x.min(): return np.zeros_like(x)
+        return (x - x.min()) / (data1_max - x.min())
+
+## Make images for video
+for n in range(num_z_stack):
+    fig = plt.figure(figsize=(12.5, 12.5), dpi=(300))
+    z = int(num_slices/2)
+    for t in range(current_tp +1 , current_tp + 1 + int(num_tps/num_z_stack)):
+        plt.clf()
+        current_frame += 1
+        current_tp += 1
+        time = 0.02*current_tp
+        print('time = ', time)
+        rgb0 = np.zeros(data[t, z, 0, :, :].shape + (3,))
+        rgb1 = np.zeros(data[t, z, 1, :, :].shape + (3,))
+        rgb0[:, :, :] = norm0(data[t, z, 0, :, :]).reshape(data.shape[-2], data.shape[-1], 1)
+        rgb1[:, :, 1] = norm1(data[t, z, 1, :, :])
+        overlay = np.clip(rgb0 + rgb1, 0, 1)
+        scale_bar = overlay[ :, :, :]
+        scale_bar[30:40, 1815:2000] = 1
+        plt.imshow(overlay)
+        plt.gca().get_xaxis().set_ticks([])
+        plt.gca().get_yaxis().set_ticks([])
+        plt.figtext(xmargin, ymargin + space, 'z=%6s$\mu$m'%('%0.2f'%(z_scale*z)),
+                    color='white', family='monospace')
+        plt.figtext(xmargin, ymargin, 't=%6ss'%('%0.2f'%time),
+                    color='white', family='monospace')
+        plt.savefig(output_filename%current_frame, bbox_inches='tight')
+    for copy in range(pause):
+        copyfile(output_filename%current_frame,
+                 output_filename%(current_frame + 1))
+        current_frame += 1
+    for z in range(int(num_slices/2), num_slices - 1, 1):        
+        plt.clf()
+        current_frame += 1
+        print('current z', z)
+        rgb0 = np.zeros(data[t, z, 0, :, :].shape + (3,))
+        rgb1 = np.zeros(data[t, z, 1, :, :].shape + (3,))
+        rgb0[:, :, :] = norm0(data[t, z, 0, :, :]).reshape(data.shape[-2], data.shape[-1], 1)
+        rgb1[:, :, 1] = norm1(data[t, z, 1, :, :])
+        overlay = np.clip(rgb0 + rgb1, 0, 1)
+        scale_bar = overlay[ :, :, :]
+        scale_bar[30:40, 1815:2000] = 1
+        plt.imshow(overlay)
+        plt.gca().get_xaxis().set_ticks([])
+        plt.gca().get_yaxis().set_ticks([])
+        plt.figtext(xmargin, ymargin + 2*space, '2 color volumetric time series',
+                    color='yellow', family='monospace')
+        plt.figtext(xmargin, ymargin + space, 'z=%6s$\mu$m'%('%0.2f'%(z_scale*z)),
+                    color='yellow', family='monospace')
+        plt.figtext(xmargin, ymargin, 't=%6ss'%('%0.2f'%time),
+                    color='white', family='monospace')
+        plt.savefig(output_filename%current_frame, bbox_inches='tight')
+        for r in range(z_slow_down_factor):
+            copyfile(output_filename%current_frame,
+                 output_filename%(current_frame + 1))
+            current_frame += 1
+    for z in range(num_slices - 1, 0, -1):
+        plt.clf()
+        current_frame += 1
+        print('current z', z)
+        rgb0 = np.zeros(data[t, z, 0, :, :].shape + (3,))
+        rgb1 = np.zeros(data[t, z, 1, :, :].shape + (3,))
+        rgb0[:, :, :] = norm0(data[t, z, 0, :, :]).reshape(data.shape[-2], data.shape[-1], 1)
+        rgb1[:, :, 1] = norm1(data[t, z, 1, :, :])
+        overlay = np.clip(rgb0 + rgb1, 0, 1)
+        scale_bar = overlay[ :, :, :]
+        scale_bar[30:40, 1815:2000] = 1
+        plt.imshow(overlay)
+        plt.gca().get_xaxis().set_ticks([])
+        plt.gca().get_yaxis().set_ticks([])
+        plt.figtext(xmargin, ymargin + 2*space, '2 color volumetric time series',
+                    color='yellow', family='monospace')
+        plt.figtext(xmargin, ymargin + space, 'z=%6s$\mu$m'%('%0.2f'%(z_scale*z)),
+                    color='yellow', family='monospace')
+        plt.figtext(xmargin, ymargin, 't=%6ss'%('%0.2f'%time),
+                    color='white', family='monospace')
+        plt.savefig(output_filename%current_frame, bbox_inches='tight')
+        for r in range(z_slow_down_factor):
+            copyfile(output_filename%current_frame,
+                 output_filename%(current_frame + 1))
+            current_frame += 1
+    for z in range(0, int(num_slices/2) + 1, 1):
+        plt.clf()
+        current_frame += 1
+        print('current z', z)
+        rgb0 = np.zeros(data[t, z, 0, :, :].shape + (3,))
+        rgb1 = np.zeros(data[t, z, 1, :, :].shape + (3,))
+        rgb0[:, :, :] = norm0(data[t, z, 0, :, :]).reshape(data.shape[-2], data.shape[-1], 1)
+        rgb1[:, :, 1] = norm1(data[t, z, 1, :, :])
+        overlay = np.clip(rgb0 + rgb1, 0, 1)
+        scale_bar = overlay[ :, :, :]
+        scale_bar[30:40, 1815:2000] = 1
+        plt.imshow(overlay)
+        plt.gca().get_xaxis().set_ticks([])
+        plt.gca().get_yaxis().set_ticks([])
+        plt.figtext(xmargin, ymargin + 2*space, '2 color volumetric time series',
+                    color='yellow', family='monospace')
+        plt.figtext(xmargin, ymargin + space, 'z=%6s$\mu$m'%('%0.2f'%(z_scale*z)),
+                    color='yellow', family='monospace')
+        plt.figtext(xmargin, ymargin, 't=%6ss'%('%0.2f'%time),
+                    color='white', family='monospace')
+        plt.savefig(output_filename%current_frame, bbox_inches='tight')
+        for r in range(z_slow_down_factor):
+            copyfile(output_filename%current_frame,
+                 output_filename%(current_frame + 1))
+            current_frame += 1
+    for copy in range(pause):
+        copyfile(output_filename%current_frame,
+                 output_filename%(current_frame + 1))
+        current_frame += 1
+    plt.close(fig)
+
+## Choose 'poster' image and copy to video location
+copyfile(os.path.join(temp_directory, 'img000014.png'),
+         os.path.join(output_directory, 'poster.png'))
+
+## Make video from images
+print("Converting images to mp4...", end='')
+convert_command = [
+   'ffmpeg', '-y',              # auto overwrite files
+   '-r', '10',                  # frame rate
+   '-f', 'image2',              # format is image sequence
+   '-i', os.path.join(temp_directory,
+                      'img%06d.png'),  # image sequence name
+   '-movflags', 'faststart',    # internet optimisation...(?)
+   '-pix_fmt', 'yuv420p',       # cross browser compatibility
+   '-vcodec', 'libx264',        # codec choice
+   '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2',  # even pixel number (important)
+   '-preset', 'veryslow',       # take time and compress to max
+   '-crf', '25',                # image quality vs file size
+   os.path.join(output_directory, 'figure.mp4')]  # output file name
+try:
+   with open('conversion_messages.txt', 'wt') as f:
+       f.write("So far, everthing's fine...\n")
+       f.flush()
+       sp.check_call(convert_command, stderr=f, stdout=f)
+       f.flush()
+   os.remove('conversion_messages.txt')
+except: # This is unlikely to be platform independent :D
+   print("MP4 conversion failed. Is ffmpeg installed?")
+   raise
+print('"figure.mp4" created')
+print('done.')
