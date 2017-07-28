a6ea56d3241c59f4715a0c7aff99c3489bf1e3b6
Alfred Millett-Sikking <amsikking@gmail.com>
2017-07-28 12:32:30 -0700

    Add files via upload

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -3,11 +3,11 @@ var big_data_output = "./../big_data_output";
 function update_figure_1() {
   var choice = document.getElementById("figure_1_choice").value;
   if ((choice === "DIC")){
-  var filename = big_data_output + "/Worm DIC/figure.mp4";
-  var postername = big_data_output + "/Worm DIC/poster.png";}
+  var filename = big_data_output + "/Worm_DIC/figure.mp4";
+  var postername = big_data_output + "/Worm_DIC/poster.png";}
   if ((choice === "TLFL")){
-  var filename = big_data_output + "/Worm TL FL/figure.mp4";
-  var postername = big_data_output + "/Worm TL FL/poster.png";}  
+  var filename = big_data_output + "/Worm_TL_FL/figure.mp4";
+  var postername = big_data_output + "/Worm_TL_FL/poster.png";}  
   var vid = document.getElementById("Figure_1_vid");
   vid.src = filename;
   vid.poster = postername;
@@ -16,7 +16,7 @@ function update_figure_1() {
 function update_figure_5() {
   var microscope = document.getElementById("figure_5_microscope").value;
   var RR = document.getElementById("figure_5_RR").value;
-  var filename = big_data_output + "/SIM target/img" + microscope + RR + ".png";
+  var filename = big_data_output + "/SIM_target/img" + microscope + RR + ".png";
   var img = document.getElementById("Figure_5_png");
   img.src = filename;
 }
@@ -63,11 +63,11 @@ function update_figure_8() {
 function update_figure_9() {
   var choice = document.getElementById("figure_9_choice").value;
   if ((choice === "3color")){
-  var filename = big_data_output + "/Yeast 3 color/figure.mp4";
-  var postername = big_data_output + "/Yeast 3 color/poster.png";}
+  var filename = big_data_output + "/Yeast_3_color/figure.mp4";
+  var postername = big_data_output + "/Yeast_3_color/poster.png";}
   if ((choice === "2color")){
-  var filename = big_data_output + "/Yeast 2 color/figure.mp4";
-  var postername = big_data_output + "/Yeast 2 color/poster.png";}  
+  var filename = big_data_output + "/Yeast_2_color/figure.mp4";
+  var postername = big_data_output + "/Yeast_2_color/poster.png";}  
   if ((choice === "amoeba")){
   var filename = big_data_output + "/Amoeba/figure.mp4";
   var postername = big_data_output + "/Amoeba/poster.png";}
@@ -78,11 +78,11 @@ function update_figure_9() {
   var filename = big_data_output + "/Spirostomum/figure.mp4";
   var postername = big_data_output + "/Spirostomum/poster.png";}
   if ((choice === "food")){
-  var filename = big_data_output + "/Spirostomum food/figure.mp4";
-  var postername = big_data_output + "/Spirostomum food/poster.png";}
+  var filename = big_data_output + "/Spirostomum_food/figure.mp4";
+  var postername = big_data_output + "/Spirostomum_food/poster.png";}
   if ((choice === "flower")){
-  var filename = big_data_output + "/Spirostomum flower/figure.mp4";
-  var postername = big_data_output + "/Spirostomum flower/poster.png";}
+  var filename = big_data_output + "/Spirostomum_flower/figure.mp4";
+  var postername = big_data_output + "/Spirostomum_flower/poster.png";}
   var vid = document.getElementById("Figure_9_vid");
   vid.src = filename;
   vid.poster = postername;
@@ -91,15 +91,15 @@ function update_figure_9() {
 function update_afigure_4() {
   var choice = document.getElementById("afigure_4_choice").value;
   if ((choice === "0.1umRR")){
-  var filename = "./images/PI/Step response - RR piezo for 0.1um - 10ms.png";} 
+  var filename = "./images/PI/Step_response-RR_piezo_for_0.1um-10ms.png";} 
   if ((choice === "0.5umRR")){
-  var filename = "./images/PI/Step response - RR piezo for 0.5um - 10ms.png";}  
+  var filename = "./images/PI/Step_response-RR_piezo_for_0.5um-10ms.png";}  
   if ((choice === "1.0umRR")){
-  var filename = "./images/PI/Step response - RR piezo for 1um - 10ms.png";}
+  var filename = "./images/PI/Step_response-RR_piezo_for_1um-10ms.png";}
   if ((choice === "15umRR")){
-  var filename = "./images/PI/Step response - RR piezo for 15um - 15ms.png";}
+  var filename = "./images/PI/Step_response-RR_piezo_for_15um-15ms.png";}
   if ((choice === "10umInsert")){
-  var filename = "./images/PI/Step response - Z piezo insert for 10um - 50ms.png";}
+  var filename = "./images/PI/Step_response-Z_piezo_insert_for_10um-50ms.png";}
   var img = document.getElementById("AFigure_4_png");
   img.src = filename;
 }
